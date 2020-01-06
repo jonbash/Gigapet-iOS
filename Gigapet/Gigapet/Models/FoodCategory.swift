388e6f00a24afffeec0cd49caf56703da2f7da9c
Jon Bash <jonbash@protonmail.com>
2020-01-06 08:55:50 -0800

    add food entry, category models

---
+++ Gigapet/Gigapet/Models/FoodCategory.swift
@@ -0,0 +1,19 @@
+//
+//  FoodCategory.swift
+//  Gigapet
+//
+//  Created by Jon Bash on 2020-01-06.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+enum FoodCategory: String {
+    case fruit
+    case vegetable
+    case wholeGrains
+    case meat
+    case dairy
+    case fatsAndOils
+    case treats
+}
