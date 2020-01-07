d8fb161273cfb7416a0732cee35cda51de252391
Jon Bash <jonbash@protonmail.com>
2020-01-06 16:49:07 -0800

    add user info coding

--- Gigapet/Gigapet/Models/UserInfo.swift
+++ Gigapet/Gigapet/Models/UserInfo.swift
@@ -8,8 +8,32 @@
 
 import Foundation
 
-struct UserInfo: Codable {
-    var uuid: UUID
-    var username: String
-    var petName: String
+struct UserInfo: Decodable {
+    var id: Int
+    let username: String
+    let password: String
+    var token: String
+
+    enum TopLevelKey: CodingKey {
+        case token
+        case saved
+    }
+
+    enum UserKey: CodingKey {
+        case id
+        case username
+        case password
+    }
+
+    init(from decoder: Decoder) throws {
+        let topContainer = try decoder.container(keyedBy: TopLevelKey.self)
+        let userContainer = try topContainer.nestedContainer(
+            keyedBy: UserKey.self,
+            forKey: .saved)
+
+        self.token = try topContainer.decode(String.self, forKey: .token)
+        self.id = try userContainer.decode(Int.self, forKey: .id)
+        self.username = try userContainer.decode(String.self, forKey: .username)
+        self.password = try userContainer.decode(String.self, forKey: .password)
+    }
 }
