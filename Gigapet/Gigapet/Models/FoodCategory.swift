33ee86dfb3bcaae2287bbdac3baf1d4568d39221
Jon Bash <jonbash@protonmail.com>
2020-01-08 12:55:04 -0800

    simplify food category

--- Gigapet/Gigapet/Models/FoodCategory.swift
+++ Gigapet/Gigapet/Models/FoodCategory.swift
@@ -8,53 +8,12 @@
 
 import Foundation
 
-enum FoodCategory {
+enum FoodCategory: String, CaseIterable {
     case fruit
     case vegetable
-    case wholeGrains
+    case wholeGrains = "Whole Grains"
     case meat
     case dairy
-    case fatsAndOils
+    case fatsAndOils = "Fats & Oils"
     case treats
-    case other(String)
-
-    var rawValue: String {
-        switch self {
-        case .fruit: return "fruit"
-        case .vegetable: return "vegetable"
-        case .wholeGrains: return "wholeGrains"
-        case .meat: return "meat"
-        case .dairy: return "dairy"
-        case .fatsAndOils: return "fatsAndOils"
-        case .treats: return "treats"
-        case .other(let type): return type
-        @unknown default: fatalError("Unknown food category")
-        }
-    }
-
-    init?(rawValue: String) {
-        if let possibleValue = PossibleValue(rawValue: rawValue) {
-            switch possibleValue {
-            case .fruit: self = .fruit
-            case .vegetable: self = .vegetable
-            case .wholeGrains: self = .wholeGrains
-            case .meat: self = .meat
-            case .dairy: self = .dairy
-            case .fatsAndOils: self = .fatsAndOils
-            case .treats: self = .treats
-            }
-        } else {
-            self = .other(rawValue)
-        }
-    }
-
-    private enum PossibleValue: String {
-        case fruit
-        case vegetable
-        case wholeGrains
-        case meat
-        case dairy
-        case fatsAndOils
-        case treats
-    }
 }
