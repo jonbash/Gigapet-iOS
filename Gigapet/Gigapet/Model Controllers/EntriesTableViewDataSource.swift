ae0e98df24886a4424829cc8c63a3e35345ca49d
Jon Bash <jonbash@protonmail.com>
2020-01-08 20:34:20 -0800

    reorganize a tad

--- Gigapet/Gigapet/Model Controllers/EntriesTableViewDataSource.swift
+++ Gigapet/Gigapet/Model Controllers/EntriesTableViewDataSource.swift
@@ -76,8 +76,6 @@ class EntriesTableViewDataSource: NSObject, UITableViewDataSource {
                 DispatchQueue.main.async {
                     if let error = result {
                         self?.delegate?.entryDeletionDidFail(withError: error)
-                    } else {
-                        tableView.deleteRows(at: [indexPath], with: .automatic)
                     }
                 }
             }
