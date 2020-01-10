6be179cc63acc02399a225072399a08827746b22
Jon Bash <jonbash@protonmail.com>
2020-01-09 16:54:04 -0800

    add equatable conformance for tests

--- Gigapet/GigapetTests/Objects+Equatable.swift
+++ Gigapet/GigapetTests/Objects+Equatable.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+@testable import Gigapet
+
+// MARK: - FoodEntryRep
+
+extension FoodEntryRepresentation: Equatable {
+    public static func == (
+        _ lhs: FoodEntryRepresentation,
+        _ rhs: FoodEntryRepresentation
+    ) -> Bool {
+        let identifierBasicallyEqual = (
+            lhs.identifier == rhs.identifier ||
+            lhs.identifier == nil ||
+            rhs.identifier == nil)
+        return (identifierBasicallyEqual &&
+            lhs.foodName == rhs.foodName &&
+            lhs.foodCategory == rhs.foodCategory &&
+            lhs.foodAmount == rhs.foodAmount &&
+            lhs.dateFed == rhs.dateFed)
+    }
+}
+
+// MARK: - UserInfo
+
+extension UserInfo: Equatable {
+    public static func == (lhs: UserInfo, rhs: UserInfo) -> Bool {
+        return lhs.id == rhs.id &&
+            lhs.petname == rhs.petname &&
+            lhs.token == rhs.token
+    }
+}
