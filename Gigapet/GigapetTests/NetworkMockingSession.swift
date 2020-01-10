a07ad7199c7166fc4eab850a00b545154fccb91a
Jon Bash <jonbash@protonmail.com>
2020-01-09 16:53:05 -0800

    add networkmockingsession to test target

---
+++ Gigapet/GigapetTests/NetworkMockingSession.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkMockingSession.swift
+//  NetworkHandler
+//
+//  Created by Michael Redig on 6/17/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import Foundation
+import NetworkHandler
+
+struct NetworkMockingSession: NetworkLoader {
+
+	// MARK: - Properties
+	let mockData: Data?
+	let mockError: Error?
+	let mockResponseCode: Int
+	var mockDelay: TimeInterval
+
+	// MARK: - Init
+	init(mockData: Data?, mockError: Error?, mockResponseCode: Int = 200, mockDelay: TimeInterval = 0.1) {
+		self.mockData = mockData
+		self.mockError = mockError
+		self.mockResponseCode = mockResponseCode
+		self.mockDelay = mockDelay
+	}
+
+	// MARK: - Public
+	public func loadData(with request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask? {
+		guard let url = request.url else { completion(nil, nil, nil); return nil }
+		let mockResponse = HTTPURLResponse(url: url, statusCode: mockResponseCode, httpVersion: "HTTP/2", headerFields: nil)
+
+		DispatchQueue.global().asyncAfter(deadline: .now() + mockDelay) {
+			completion(self.mockData, mockResponse, self.mockError)
+		}
+
+		return nil
+	}
+
+
+}
