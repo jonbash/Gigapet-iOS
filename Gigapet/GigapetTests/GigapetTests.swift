865c2d99686a96aea39ef193cdf1897627ba12e4
Jon Bash <jonbash@protonmail.com>
2020-01-09 16:54:57 -0800

    further setup for tests

--- Gigapet/GigapetTests/GigapetTests.swift
+++ Gigapet/GigapetTests/GigapetTests.swift
@@ -12,17 +12,48 @@ import XCTest
 class GigapetTests: XCTestCase {
     lazy var entryController = FoodEntryController(user: user)
     var user = UserInfo(id: 1, token: "testToken", petname: "Kilmonger")
+    var entryRep = FoodEntryRepresentation(
+        foodCategory: .dairy,
+        foodName: "Milk",
+        foodAmount: 3,
+        dateFed: Date(timeIntervalSinceReferenceDate: 0),
+        identifier: 7)
+    lazy var entryRepData: Data? = try? self.encodeEntryRep()
+    var context = CoreDataStack.shared.container.newBackgroundContext()
+    lazy var returnData: Data? = try? JSONEncoder().encode([entryRep])
 
-    override func setUp() {
-        entryController = FoodEntryController(user: user)
+
+    // MARK: - Helpers
+
+    func setUpEntryController(withError: Bool) {
+        entryController = FoodEntryController(
+            user: user,
+            loader: NetworkMockingSession(
+                mockData: returnData,
+                mockError: withError ? GigapetError(text: "Ruh roh") : nil ))
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func encodeEntryRep() throws -> Data {
+        return try JSONEncoder().encode(entryRep)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func getEntryRepFromData() throws -> FoodEntryRepresentation? {
+        guard let data = entryRepData else { return nil }
+
+        return try JSONDecoder().decode(FoodEntryRepresentation.self, from: data)
+    }
+
+    func newEntry() -> FoodEntry {
+        return FoodEntry(
+            category: .meat,
+            foodName: "Steak",
+            foodAmount: 10,
+            dateFed: Date(),
+            identifier: 9,
+            context: context)
+    }
+
+    func requestExpectation() -> XCTestExpectation {
+        return expectation(description: "Finished mock network request")
     }
 }
