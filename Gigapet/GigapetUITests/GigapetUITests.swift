1ad94b1241a8bdb32b873fb6ec532055e8923b3e
Jon Bash <jonbash@protonmail.com>
2020-01-09 19:00:48 -0800

    add first ui test

--- Gigapet/GigapetUITests/GigapetUITests.swift
+++ Gigapet/GigapetUITests/GigapetUITests.swift
@@ -9,25 +9,74 @@
 import XCTest
 
 class GigapetUITests: XCTestCase {
-//    override func setUp() {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//
-//        // In UI tests it is usually best to stop immediately when a failure occurs.
-//        continueAfterFailure = false
-//
-//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-//    }
-//
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use recording to get started writing UI tests.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
+
+    var app: XCUIApplication!
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+
+        app = XCUIApplication()
+        app.launchArguments = ["UITesting"]
+        app.launch()
+    }
+
+    func testAuthScreen() {
+        let homeNavBar = app.navigationBars["Home"]
+        let logOutButton = homeNavBar.buttons["Log Out"]
+
+        if homeNavBar.exists && logOutButton.exists {
+            logOutButton.tap()
+        }
+
+        let welcomeHeader = app.staticTexts["Welcome to Lambdi Pet"]
+        let userNameLabel = app.staticTexts["User Name"]
+        let petNameLabel = app.staticTexts["Pet Name"]
+        let passwordLabel = app.staticTexts["Password"]
+
+        let userNameField = app.textFields["User Name"]
+        let petNameField = app.textFields["Pet Name"]
+        let passwordField = app.textFields["Password"]
+
+        let authTypeControl = app.segmentedControls.element
+        let loginSegment = authTypeControl.buttons["Log In"]
+        let registerSegment = authTypeControl.buttons["Register"]
+
+        XCTAssert(welcomeHeader.exists)
+        XCTAssert(userNameLabel.exists)
+        XCTAssert(passwordLabel.exists)
+        XCTAssert(petNameLabel.exists)
+
+        XCTAssert(authTypeControl.exists)
+        XCTAssert(loginSegment.exists)
+        XCTAssert(registerSegment.exists)
+
+        XCTAssert(userNameField.exists)
+        XCTAssert(petNameField.exists)
+        XCTAssert(passwordField.exists)
+
+        loginSegment.tap()
+
+        XCTAssertFalse(petNameLabel.exists && petNameLabel.isHittable)
+        XCTAssertFalse(petNameField.exists && petNameField.isHittable)
+
+        userNameField.tap()
+        userNameField.typeText("test")
+        passwordField.tap()
+        passwordField.typeText("password")
+
+        app.buttons.containing(.staticText, identifier: "Log In").element.tap()
+
+        let loggedIn = expectation(
+            for: NSPredicate(format: "exists == 1"),
+            evaluatedWith: homeNavBar,
+            handler: nil)
+        wait(for: [loggedIn], timeout: 5)
+
+        XCTAssert(homeNavBar.exists)
+    }
+
+    func testExample() {
+
+    }
 }
